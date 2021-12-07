09af1aedb61107ed5cf4de8fd8ce0658c828dc87
davidyoon <qqpo12@naver.com>
2021-12-07 20:06:33 +0900

    add writeDiary UILayout, ViewController

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by David Yoon on 2021/12/07.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
