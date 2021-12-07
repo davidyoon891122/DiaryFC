3c802672a807aa9411a26a0a9016392f7d025720
davidyoon <qqpo12@naver.com>
2021-12-07 20:12:11 +0900

    add DetailView UILayout and DetailViewController

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by David Yoon on 2021/12/07.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
